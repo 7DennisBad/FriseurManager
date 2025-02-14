b215f6b4b0cb670f78feb228a7f66fbfb0a57d43
Marc Trautwein <marctrautwein15@gmail.com>
2025-02-14 22:24:31 +0100

    test commit on new branch

--- untitled/js/login.js
+++ untitled/js/login.js
@@ -5,10 +5,10 @@ function checkLogin(){
   let userNameIn = document.getElementById("userName").value;
   let userPassIn = document.getElementById("userPass").value;
   if (userNameIn !== userName || userPassIn !== userPass){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
     alert("username or password incorrect!");
   } else {
     window.location.href = "../html/minigame.html";
     alert("succesfull login! waiting for DataBase integration!\nEnjoy the mini-games in the meantime :)");
-
   }
 }
