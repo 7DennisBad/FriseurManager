b847257a8a53fcf6fe0dbc97fd346e603ff04687
Marc Trautwein <marctrautwein15@gmail.com>
2025-02-15 00:50:53 +0100

    testing login with json file, this needs to be SQL-Database in the future

--- untitled/js/login.js
+++ untitled/js/login.js
@@ -1,14 +1,43 @@
-const userName = "Owner";
-const userPass = "AdminPass";
 
-function checkLogin(){
-  let userNameIn = document.getElementById("userName").value;
-  let userPassIn = document.getElementById("userPass").value;
-  if (userNameIn !== userName || userPassIn !== userPass){
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-    alert("username or password incorrect!");
+function proceedLogin(){
+  console.log("Login successful!");
+  window.location.href = "../html/minigame.html";
+}
+
+function showError(){
+  alert("Incorrect username or password!");
+  //window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+  window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+}
+
+
+
+function checkLoginData(userNameIn, userPassIn, users) {
+  const user = users.find(user => user.userName === userNameIn);
+  if (user) {
+    if (user.userPass === userPassIn) {
+      proceedLogin();
+    } else {
+      showError();
+    }
   } else {
-    window.location.href = "../html/minigame.html";
-    alert("succesfull login! waiting for DataBase integration!\nEnjoy the mini-games in the meantime :)");
+    showError();
   }
 }
+
+
+function loadJson(userNameIn, userPassIn) {
+  fetch("../json/Logins.json")
+    .then(response => response.json())
+    .then(users => checkLoginData(userNameIn, userPassIn, users))
+    .catch(error => console.error("Error loading JSON:", error));
+}
+
+
+function attemptLogin() {
+  let userNameIn = document.getElementById("userName").value;
+  let userPassIn = document.getElementById("userPass").value;
+  loadJson(userNameIn, userPassIn);
+}
+
